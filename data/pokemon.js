9371f58ed5d0bebfccaf9fbeeca961d257774124
SSSpac <saifeisa820@gmail.com>
2025-04-06 15:13:44 +0200

    pokemon world project

---
+++ data/pokemon.js
@@ -0,0 +1,27 @@
+module.exports = {
+    kanto: [
+      {
+        id: 1,
+        name: "Bulbasaur",
+        type: ["Grass", "Poison"],
+        description: "A strange seed was planted on its back at birth. The plant sprouts and grows with this Pokémon.",
+        image: "bulbasaur.png"
+      },
+      {
+        id: 25,
+        name: "Pikachu",
+        type: ["Electric"],
+        description: "When several of these Pokémon gather, their electricity could build and cause lightning storms.",
+        image: "pikachu.png"
+      }
+    ],
+    johto: [
+      {
+        id: 152,
+        name: "Chikorita",
+        type: ["Grass"],
+        description: "A sweet aroma gently wafts from the leaf on its head. It is docile and loves to soak up the sun's rays.",
+        image: "chikorita.png"
+      }
+    ]
+  };
